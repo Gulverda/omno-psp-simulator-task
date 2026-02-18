144bb78b1d6e10523867699916c2f650135366ca
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2026-02-18 11:19:59 +0400

    Add: dockerized postgres and initial sql migration

---
+++ db/migrations/001_init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS transactions (
+  id UUID PRIMARY KEY,
+  order_id TEXT NOT NULL,
+  psp_transaction_id TEXT UNIQUE,
+  status TEXT NOT NULL,
+  amount INTEGER NOT NULL,
+  currency TEXT NOT NULL,
+  final_amount INTEGER,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS webhook_events (
+  id UUID PRIMARY KEY,
+  psp_transaction_id TEXT NOT NULL,
+  status TEXT NOT NULL,
+  final_amount INTEGER,
+  payload_hash TEXT NOT NULL UNIQUE,
+  received_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
